cc85dfceda4c0b1cbf33d37894c51b7dd04c0f46
JaysonBH <jhairston@pivotal.io>
2020-08-08 06:04:19 -0400

    Enable stopping instances of 3 zones

--- gcp/platform-vms-stop/stop-platform-vms.sh
+++ gcp/platform-vms-stop/stop-platform-vms.sh
@@ -2,7 +2,21 @@
 
 source config
 
-echo Getting a list of all platform VM\'s with tag $TAG
-gcloud compute instances list --format=json --filter="tags.items=$VM_TAG_NAME" | jq -r .[].name
+export FILTER="tags.items=$VM_TAG_NAME"
+
+echo "Initiating STOP for instances within zone=$ZONE1 with vm tag=$VM_TAG_NAME"
+gcloud --format=json compute instances list \
+  --filter="(zone:( $ZONE2 ))($FILTER)" | jq \
+   -r .[].name | xargs gcloud compute instances stop --zone=$ZONE2 --async
+
+echo "Initiating STOP for instances within zone=$ZONE2 with vm tag=$VM_TAG_NAME"
+gcloud --format=json compute instances list \
+  --filter="(zone:( $ZONE2 ))($FILTER)" | jq \
+   -r .[].name | xargs gcloud compute instances stop --zone=$ZONE2 --async
+
+echo "Initiating STOP for instances within zone=$ZONE3 with vm tag=$VM_TAG_NAME"
+gcloud --format=json compute instances list \
+  --filter="(zone:( $ZONE3 ))($FILTER)" | jq \
+   -r .[].name | xargs gcloud compute instances stop --zone=$ZONE3 --async
 
 exit 0
