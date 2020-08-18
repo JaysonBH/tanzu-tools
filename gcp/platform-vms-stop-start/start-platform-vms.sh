0c05cd8db6ab5fc6ea3faa164436222731b4dc97
JaysonBH <jhairston@pivotal.io>
2020-08-18 10:22:18 -0400

    Add Starting up the Bosh Director before CF instances

--- gcp/platform-vms-stop-start/start-platform-vms.sh
+++ gcp/platform-vms-stop-start/start-platform-vms.sh
@@ -3,6 +3,14 @@
 source config
 
 export FILTER_TAGS="tags.items=$VM_TAG_NAME"
+export FILTER_LABEL_BOSH_DIRECTOR="labels.deployment:p-bosh"
+
+echo "Initiating START for The Bosh Director within zone=$ZONE1 with vm tag=$VM_TAG_NAME"
+gcloud --format=json compute instances list \
+  --filter="(zone:( $ZONE1 ))($FILTER_TAGS)($FILTER_LABEL_BOSH_DIRECTOR)" | jq \
+   -r .[].name | xargs gcloud compute instances start --zone=$ZONE1
+
+sleep 10
 
 echo "Initiating START for instances within zone=$ZONE1 with vm tag=$VM_TAG_NAME"
 gcloud --format=json compute instances list \
